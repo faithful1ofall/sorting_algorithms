b4f19659225e9e586e3d2abbaa19d4072f9c3a1b
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-01-19 23:03:51 +0100

    Update deck.h

--- deck.h
+++ deck.h
@@ -47,6 +47,10 @@ typedef struct deck_node_s
 
 void sort_deck(deck_node_t **deck);
 
-
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
 
 #endif /* DECK_H */
