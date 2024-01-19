29895b9442c36bcfa07ee6d746787a62cac9ac2f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-01-19 20:36:50 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -35,15 +35,19 @@ typedef struct listint_s
 
 /* helper functions */
 void print_array(const int *array, size_t size);
-void print_list(const flistint_t *list);
+void print_list(const listint_t *list);
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 
 /* Sorting algoritms Functions*/
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(flistint_t **list);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(flistint_t **list);
+void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
