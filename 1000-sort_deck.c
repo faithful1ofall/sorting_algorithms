b54ca57001b565da967292bf41955562862aa2df
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-01-19 23:03:16 +0100

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,11 +1,5 @@
 #include "deck.h"
 
-int _strcmp(const char *s1, const char *s2);
-char get_value(deck_node_t *card);
-void insertion_sort_deck_kind(deck_node_t **deck);
-void insertion_sort_deck_value(deck_node_t **deck);
-void sort_deck(deck_node_t **deck);
-
 /**
  * _strcmp - Compares two strings.
  * @s1: The first string to be compared.
